35ff3e891e65e288c8ac5d9d44ae738c7ce1ca7a
John Nabil Hanna <Johnnabil67@gmail.com>
2024-10-03 17:45:16 +0200

    Portfolio, basics added.

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector(".meny-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
