41023248525b93b4b3843e074fdba75d69561dd8
John Nabil Hanna <Johnnabil67@gmail.com>
2024-10-10 14:58:34 +0200

    more responsive for smaller devices

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function toggleMenu() {
-    const menu = document.querySelector(".meny-links");
+    const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
